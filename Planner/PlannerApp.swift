39841109bf670c9da35fc8019bd9907a3b9409b6
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-04-06 21:38:46 +0800

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Darma Wiryanata on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
