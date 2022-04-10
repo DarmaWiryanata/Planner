9583faeec216ba98e151544ede0a9ae07a9dafed
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-04-10 23:12:47 +0800

    New no items view

--- Planner/Views/PlannerView.swift
+++ Planner/Views/PlannerView.swift
@@ -17,12 +17,47 @@ struct PlannerView: View {
     
     @State private var showSheet: Bool = false
     @State private var shareSheet = false
+    @State private var animate: Bool = false
 
     var body: some View {
         NavigationView {
             ZStack {
                 if planViewModel.plans.isEmpty {
-                    Text("No items available")
+                    ScrollView {
+                        VStack(spacing: 10) {
+                            Text("There are no item!")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                            
+                            Text("Are you a productive person? I think you should click the add button and add a bunch of item to your plan!")
+                                .padding(.bottom, 20)
+                            
+                            Text("Add Something 🥳")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                                .frame(height: 55)
+                                .frame(maxWidth: .infinity)
+                                .background(.blue)
+                                .cornerRadius(10)
+                                .onTapGesture {
+                                    createPlan()
+                                }
+                            
+                            .padding(.horizontal, animate ? 30 : 50)
+                            .shadow(
+                                color: .blue.opacity(0.7),
+                                radius: animate ? 30 : 10,
+                                x: 0,
+                                y: animate ? 50 : 30
+                            )
+                            .scaleEffect(animate ? 1.1 : 1.0)
+                            .offset(y: animate ? -7 : 0)
+                        }
+                        .frame(maxWidth: 400)
+                        .multilineTextAlignment(.center)
+                        .padding(40)
+                    .onAppear(perform: addAnimation)
+                    }
                 } else {
                     List {
                         ForEach(planViewModel.plans) { item in
@@ -102,6 +137,19 @@ struct PlannerView: View {
             return " at \(sharePlan?.date?.formatDateTime() ?? "")"
         } else {
             return ""
+        }
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
         }
     }
     
